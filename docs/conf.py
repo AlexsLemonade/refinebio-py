7e2306bff9a7b9c00dca6a6fe2dc7edb6609f8b3
samuel-tate <samuel.tate@outlook.com>
2020-12-15 11:00:11 -0500

    install dependencies for docs and added github make command

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,8 @@ sys.path.insert(0, os.path.abspath('..'))
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'sphinx.ext.githubpages'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
