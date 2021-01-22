f432f693622d221be0207b75d3d9cb48534a27f5
samuel-tate <samuel.tate@outlook.com>
2021-01-22 12:53:03 -0500

    add script tests, add dataset non-downloadable test, expose formatted message to errors

--- pyrefinebio/script.py
+++ pyrefinebio/script.py
@@ -3,6 +3,8 @@ import json
 
 import pyrefinebio.high_level_functions as hlf 
 
+from pyrefinebio.exceptions import DownloadError
+
 @click.group()
 def cli():
     """RefineBio CLI
@@ -54,9 +56,9 @@ def help(entity=None):
 
 
 @cli.command()
-@click.option("--path", help="Path that the dataset should be downloaded to")
-@click.option("--email-address", help="The email that will be contacted with info related to the Dataset")
-@click.option("--dataset-dict", default=None, type=DictParamType(), help="A fully formed Dataset `data` attribute. Use this parameter if you want to specify specific Samples for your Dataset")
+@click.option("--path", required=True, help="Path that the dataset should be downloaded to")
+@click.option("--email-address", required=True, help="The email that will be contacted with info related to the Dataset")
+@click.option("--dataset-dict", default=None, type=DictParamType(), help="A fully formed Dataset `data` attribute. Use this parameter if you want to specify specific Samples for your Dataset. Ex: '{\"GSE74410\": [\"GSM1919903\"]}' ")
 @click.option("--experiments", default=None, type=ListParamType(), help="A space separated list of experiment accession codes. Ex: 'SRP051449 GSE44421 GSE44422'")
 @click.option("--aggregation", default="EXPERIMENT", type=click.Choice(("EXPERIMENT", "SPECIES", "ALL"), case_sensitive=True), help="How the Dataset should be aggregated")
 @click.option("--transformation", default="NONE", type=click.Choice(("NONE", "MINMAX", "STANDARD"), case_sensitive=True), help="The transformation for the Dataset")
@@ -74,21 +76,24 @@ def download_dataset(
     Automatically constructs a Dataset, processes it, waits for it
     to finish processing, then downloads it to the path specified.
     """
-    hlf.download_dataset(
-        path,
-        email_address,
-        dataset_dict,
-        experiments,
-        aggregation,
-        transformation,
-        skip_quantile_normalization
-    )
+    try:
+        hlf.download_dataset(
+            path,
+            email_address,
+            dataset_dict,
+            experiments,
+            aggregation,
+            transformation,
+            skip_quantile_normalization
+        )
+    except DownloadError as e:
+        raise click.ClickException(e.message)
 
 
 @cli.command()
 @click.option("--path", help="Path that the Compendium should be downloaded to")
 @click.option("--organism", help="The name fo the Organism for the Compendium you want to download")
-@click.option("--quant-sf-only", default=False, help="True for RNA-seq Sample Compendium results or False for quantile normalized")
+@click.option("--quant-sf-only", default=False, type=click.BOOL, help="True for RNA-seq Sample Compendium results or False for quantile normalized")
 def download_compendium(
     path,
     organism,
@@ -97,11 +102,14 @@ def download_compendium(
     """
     Download a Compendium for the specified organism.
     """
-    hlf.download_compendium(
-        path,
-        organism,
-        quant_sf_only
-    )
+    try:
+        hlf.download_compendium(
+            path,
+            organism,
+            quant_sf_only
+        )
+    except DownloadError as e:
+        raise click.ClickException(e.message)
 
 
 @cli.command()
@@ -112,4 +120,7 @@ def download_quandfile_compendium(path, organism):
     Download a Compendium for the specified organism.
     This command will always download RNA-seq Sample Compedium results.
     """
-    hlf.download_quandfile_compendium(path, organism)
+    try:
+        hlf.download_quandfile_compendium(path, organism)
+    except DownloadError as e:
+        raise click.ClickException(e.message)
---
+++ tests/test_script.py
@@ -0,0 +1,129 @@
+import unittest
+
+from unittest.mock import Mock, patch
+from click.testing import CliRunner
+
+from pyrefinebio.script import cli
+
+class ScriptTests(unittest.TestCase):
+
+    def setUp(self):
+        self.runner = CliRunner()
+
+
+    @patch("pyrefinebio.script.hlf.download_dataset")
+    def test_download_dataset_dataset_dict(self, mock_download_dataset):
+        self.runner.invoke(
+            cli,
+            [
+                "download-dataset",
+                "--path",          "./test.zip",
+                "--email-address", "foo@bar.net",
+                "--dataset-dict",  '{"foo": ["bar"]}'
+            ]
+        )
+
+        mock_download_dataset.assert_called_with(
+            "./test.zip",
+            "foo@bar.net",
+            {"foo": ["bar"]},
+            None,
+            "EXPERIMENT",
+            "NONE",
+            False
+        )
+
+
+    @patch("pyrefinebio.script.hlf.download_dataset")
+    def test_download_dataset_experiments(self, mock_download_dataset):
+        self.runner.invoke(
+            cli,
+            [
+                "download-dataset",
+                "--path",          "./test.zip",
+                "--email-address", "foo@bar.net",
+                "--experiments",  "foo bar baz"
+            ]
+        )
+
+        mock_download_dataset.assert_called_with(
+            "./test.zip",
+            "foo@bar.net",
+            None,
+            ["foo", "bar", "baz"],
+            "EXPERIMENT",
+            "NONE",
+            False
+        )
+
+    
+    def test_download_dataset_both(self):
+        result = self.runner.invoke(
+            cli,
+            [
+                "download-dataset",
+                "--path",          "./test.zip",
+                "--email-address", "foo@bar.net",
+                "--dataset-dict",  '{"foo": ["bar"]}',
+                "--experiments",  "foo bar baz"
+            ]
+        )
+
+        self.assertEqual(result.exit_code, 1)
+        self.assertEqual(
+            result.output,
+            "Error: Unable to download Dataset\nYou should either provide dataset_dict or experiments but not both\n"
+        )
+
+
+    @patch("pyrefinebio.script.hlf.download_compendium")
+    def test_download_compendium(self, mock_download_compendium):
+        self.runner.invoke(
+            cli,
+            [
+                "download-compendium",
+                "--path",          "./test.zip",
+                "--organism",      "foo",
+                "--quant-sf-only", "true"
+            ]
+        )
+
+        mock_download_compendium.assert_called_with(
+            "./test.zip",
+            "foo",
+            True
+        )
+
+
+    @patch("pyrefinebio.script.hlf.download_quandfile_compendium")
+    def test_download_compendium(self, mock_download_q_compendium):
+        self.runner.invoke(
+            cli,
+            [
+                "download-quandfile-compendium",
+                "--path",          "./test.zip",
+                "--organism",      "foo"
+            ]
+        )
+
+        mock_download_q_compendium.assert_called_with(
+            "./test.zip",
+            "foo"
+        )
+
+    def test_download_compendium_bad_organism(self):
+        result = self.runner.invoke(
+            cli,
+            [
+                "download-compendium",
+                "--path",          "./test.zip",
+                "--organism",      "foo",
+            ]
+        )
+
+        self.assertEqual(result.exit_code, 1)
+        self.assertEqual(
+            result.output,
+            "Error: Unable to download Compendium\nCould not find any Compendium with organism name, foo and quant_sf_only, False\n"
+        )
+
